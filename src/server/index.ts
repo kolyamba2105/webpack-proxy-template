b884189cb954b353f9bbbda573b89250703929e7
kolyamba2105 <haravetskyy@gmail.com>
2023-04-23 00:23:48 +0300

    Update server setup

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,14 +2,22 @@ import express from "express";
 import morgan from "morgan";
 import path from "path";
 
-const publicPath =
-  process.env.NODE_ENV === "production"
-    ? path.join(__dirname, "public")
-    : path.join(__dirname, "..", "..", "public");
+function api(app: express.Express) {
+  return app
+    .use(morgan("dev"))
+    .use(express.Router().use("/api", (_, res) => res.status(200).send({ value: 42 })));
+}
 
-express()
-  .use(morgan("dev"))
-  .use(express.Router().use("/api", (_, res) => res.status(200).send({ value: 42 })))
-  .use(express.static(publicPath))
-  .get("/", (_, res) => res.sendFile(path.join(publicPath, "index.html")))
-  .listen(8080, () => console.log("Listening on port 8080..."));
+function client(app: express.Express) {
+  const publicPath = process.env.NODE_ENV === "production" && path.join(__dirname, "public");
+
+  if (publicPath) {
+    return app
+      .use(express.static(publicPath))
+      .get("/", (_, res) => res.sendFile(path.join(publicPath, "index.html")));
+  }
+
+  return app.get("/", (_, res) => res.redirect("http://localhost:3000"));
+}
+
+client(api(express())).listen(8080, () => console.log("Listening on port 8080..."));
